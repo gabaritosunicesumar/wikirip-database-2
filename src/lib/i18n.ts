5e4e6a1aeaea7430729ae72d1b2164fe6bd15736
gabaritosunicesumar <gabaritosunicesumar@protonmail.com>
2025-10-30 04:41:33 -0300

    Initial commit

---
+++ src/lib/i18n.ts
@@ -0,0 +1,17 @@
+import { browser } from "$app/environment";
+import { init, register } from "svelte-i18n";
+
+const fallback = "pt-BR";
+
+register("en", () => import("$i18n/en.json"));
+register("zh-TW", () => import("$i18n/zh-TW.json"));
+register("zh-CN", () => import("$i18n/zh-CN.json"));
+register("es-ES", () => import("$i18n/es-ES.json"));
+register("es-MX", () => import("$i18n/es-MX.json"));
+register("ja", () => import("$i18n/ja.json"));
+register("pt-BR", () => import("$i18n/pt-BR.json"));
+
+init({
+	fallbackLocale: fallback,
+	initialLocale: browser ? window.navigator.language : fallback,
+});
